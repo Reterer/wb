78007c1c00ecc94b3cd5078a21ea0fbde85db890
Егор Суханов <egor-suhanov2012@yandex.ru>
2023-08-05 14:25:06 +0300

    add select/insert q

---
+++ l0/app/models/order.go
@@ -0,0 +1,304 @@
+package models
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Order struct {
+	dbId              int64    `json:"-"`
+	Uid               string   `json:"order_uid"`
+	TrackNumber       string   `json:"track_number"`
+	Entry             string   `json:"entry"`
+	Delivery          Delivery `json:"delivery"`
+	Payment           Payment  `json:"payment"`
+	Items             []Item   `json:"items"`
+	Locale            string   `json:"locale"`
+	InternalSignature string   `json:"internal_signature"`
+	CustomerId        string   `json:"customer_id"`
+	DeliveryService   string   `json:"delivery_service"`
+	Shardkey          string   `json:"shardkey"`
+	SmId              int      `json:"sm_id"`
+	DateCreated       string   `json:"date_created"`
+	OofShard          string   `json:"oof_shard"`
+}
+
+type Delivery struct {
+	dbId    int64  `json:"-"`
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Zip     string `json:"zip"`
+	City    string `json:"city"`
+	Address string `json:"address"`
+	Region  string `json:"region"`
+	Email   string `json:"email"`
+}
+
+type Payment struct {
+	dbId         int64  `json:"-"`
+	Transaction  string `json:"transaction"`
+	RequestId    string `json:"request_id"`
+	Currency     string `json:"currency"`
+	Provider     string `json:"provider"`
+	Amount       int    `json:"amount"`
+	PaymentDt    int    `json:"payment_dt"`
+	Bank         string `json:"bank"`
+	DeliveryCost int    `json:"delivery_cost"`
+	GoodsTotal   int    `json:"goods_total"`
+	CustomFee    int    `json:"custom_fee"`
+}
+
+type Item struct {
+	dbId        int64  `json:"-"`
+	ChrtId      int    `json:"chrt_id"`
+	TrackNumber string `json:"track_number"`
+	Price       int    `json:"price"`
+	Rid         string `json:"rid"`
+	Name        string `json:"name"`
+	Sale        int    `json:"sale"`
+	Size        string `json:"size"`
+	TotalPrice  int    `json:"total_price"`
+	NmId        int    `json:"nm_id"`
+	Brand       string `json:"brand"`
+	Status      int    `json:"status"`
+}
+
+type CachedOrderModel struct {
+	db    *sql.DB
+	cache map[string]Order
+}
+
+func MakeCachedOrderModel() (*CachedOrderModel, error) {
+	connStr := "host=127.0.0.1 user=serviceuser password=servicepassword dbname=servicedb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	model := CachedOrderModel{
+		db:    db,
+		cache: make(map[string]Order),
+	}
+
+	// TODO INIT CACHE
+
+	return &model, nil
+}
+
+func (c *CachedOrderModel) Insert(order Order) error {
+	/*
+		1. Вставить основную информацию об ордере
+		2. Получить id нового ордера
+		3. Вставить Оплату и Доставку
+		4. Вставить Товары
+
+		5. Сохранить в кэш
+	*/
+
+	// TODO обернуть в транзацкию
+	// TODO проверить на дубликаты
+
+	// Вставка основной информации об заказе и получение id записи в БД
+	qInsertOrder := `INSERT INTO orders(
+		order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id`
+
+	err := c.db.QueryRow(qInsertOrder,
+		order.Uid,
+		order.TrackNumber,
+		order.Entry,
+		order.Locale,
+		order.InternalSignature,
+		order.CustomerId,
+		order.DeliveryService,
+		order.Shardkey,
+		order.SmId,
+		order.DateCreated,
+		order.OofShard,
+	).Scan(&order.dbId)
+	if err != nil {
+		return err
+	}
+
+	// Вставка Доставки
+	qInsertDelivery := `INSERT INTO deliveries(
+		order_id, name, phone, zip, city, address, region, email)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+
+	_, err = c.db.Exec(qInsertDelivery,
+		order.dbId,
+		order.Delivery.Name,
+		order.Delivery.Phone,
+		order.Delivery.Zip,
+		order.Delivery.City,
+		order.Delivery.Address,
+		order.Delivery.Region,
+		order.Delivery.Email)
+	if err != nil {
+		return err
+	}
+
+	// Вставка Оплаты
+	qInsertPayment := `INSERT INTO payments(
+		order_id, transaction, request_id, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+
+	_, err = c.db.Exec(qInsertPayment,
+		order.dbId,
+		order.Payment.Transaction,
+		order.Payment.RequestId,
+		order.Payment.Currency,
+		order.Payment.Provider,
+		order.Payment.Amount,
+		order.Payment.PaymentDt,
+		order.Payment.Bank,
+		order.Payment.DeliveryCost,
+		order.Payment.GoodsTotal,
+		order.Payment.CustomFee)
+	if err != nil {
+		return err
+	}
+
+	// Вставка Товаров
+	qInsertItem := `INSERT INTO items(
+		order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`
+
+	for _, item := range order.Items {
+		// TODO Делать меньше запросов вставки
+		_, err = c.db.Exec(qInsertItem,
+			order.dbId,
+			item.ChrtId,
+			item.TrackNumber,
+			item.Price,
+			item.Rid,
+			item.Name,
+			item.Sale,
+			item.Size,
+			item.TotalPrice,
+			item.NmId,
+			item.Brand,
+			item.Status)
+		if err != nil {
+			return err
+		}
+	}
+
+	// TODO вставить в кеш
+
+	return nil
+}
+
+func (c *CachedOrderModel) GetByUid(uid string) (*Order, error) {
+	/*
+		0. Проверить в кэше
+
+		1. Получить информацию о заказе, доставке, оплате
+		2. Получить информацию о товарах
+		Считать в Order
+
+		5. Сохранить в кэш
+	*/
+	// TODO Проверка в кэше
+	// TODO транзакция?
+	var order Order
+
+	// Заказ
+	// Я не использовал INNER JOIN, потому что не хочу всё в одну кучу мешать.
+	// В будущем можно будет заменить, что бы делать меньше запросов.
+
+	qSelectOrder := `SELECT id, order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard
+		FROM orders WHERE order_uid = $1`
+	err := c.db.QueryRow(qSelectOrder, uid).Scan(
+		&order.dbId,
+		&order.Uid,
+		&order.TrackNumber,
+		&order.Entry,
+		&order.Locale,
+		&order.InternalSignature,
+		&order.CustomerId,
+		&order.DeliveryService,
+		&order.Shardkey,
+		&order.SmId,
+		&order.DateCreated,
+		&order.OofShard)
+	if err != nil {
+		return nil, err
+	}
+
+	// Доставка
+	qSelectDelivery := `SELECT id, name, phone, zip, city, address, region, email
+		FROM deliveries WHERE order_id = $1`
+	err = c.db.QueryRow(qSelectDelivery, order.dbId).Scan(
+		&order.Delivery.dbId,
+		&order.Delivery.Name,
+		&order.Delivery.Phone,
+		&order.Delivery.Zip,
+		&order.Delivery.City,
+		&order.Delivery.Address,
+		&order.Delivery.Region,
+		&order.Delivery.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	// Оплата
+	qSelectPayment := `SELECT id, transaction, request_id, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee
+		FROM payments WHERE order_id = $1`
+	err = c.db.QueryRow(qSelectPayment, order.dbId).Scan(
+		&order.Payment.dbId,
+		&order.Payment.Transaction,
+		&order.Payment.RequestId,
+		&order.Payment.Currency,
+		&order.Payment.Provider,
+		&order.Payment.Amount,
+		&order.Payment.PaymentDt,
+		&order.Payment.Bank,
+		&order.Payment.DeliveryCost,
+		&order.Payment.GoodsTotal,
+		&order.Payment.CustomFee)
+	if err != nil {
+		return nil, err
+	}
+
+	// Товары
+	qSelectItems := `SELECT id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status
+		FROM items WHERE order_id = $1`
+	rows, err := c.db.Query(qSelectItems, order.dbId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	order.Items = make([]Item, 0)
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(
+			&item.dbId,
+			&item.ChrtId,
+			&item.TrackNumber,
+			&item.Price,
+			&item.Rid,
+			&item.Name,
+			&item.Sale,
+			&item.Size,
+			&item.TotalPrice,
+			&item.NmId,
+			&item.Brand,
+			&item.Status,
+		)
+		if err != nil {
+			return nil, err
+		}
+		order.Items = append(order.Items, item)
+	}
+
+	// TODO добавить в кэш
+	return &order, nil
+}
+
+func (c *CachedOrderModel) Close() {
+	if c != nil {
+		c.db.Close()
+	}
+}
---
+++ l0/app/models/order_test.go
@@ -0,0 +1,245 @@
+package models
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/go-test/deep"
+)
+
+var OrderJson1 = []byte(`{
+    "order_uid": "b563feb7b2b84b6test",
+    "track_number": "WBILMTESTTRACK",
+    "entry": "WBIL",
+    "delivery": {
+        "name": "Test Testov",
+        "phone": "+9720000000",
+        "zip": "2639809",
+        "city": "Kiryat Mozkin",
+        "address": "Ploshad Mira 15",
+        "region": "Kraiot",
+        "email": "test@gmail.com"
+    },
+    "payment": {
+        "transaction": "b563feb7b2b84b6test",
+        "request_id": "",
+        "currency": "USD",
+        "provider": "wbpay",
+        "amount": 1817,
+        "payment_dt": 1637907727,
+        "bank": "alpha",
+        "delivery_cost": 1500,
+        "goods_total": 317,
+        "custom_fee": 0
+    },
+    "items": [
+        {
+            "chrt_id": 9934930,
+            "track_number": "WBILMTESTTRACK",
+            "price": 453,
+            "rid": "ab4219087a764ae0btest",
+            "name": "Mascaras",
+            "sale": 30,
+            "size": "0",
+            "total_price": 317,
+            "nm_id": 2389212,
+            "brand": "Vivienne Sabo",
+            "status": 202
+        }
+    ],
+    "locale": "en",
+    "internal_signature": "",
+    "customer_id": "test",
+    "delivery_service": "meest",
+    "shardkey": "9",
+    "sm_id": 99,
+    "date_created": "2021-11-26T06:22:19Z",
+    "oof_shard": "1"
+}`)
+
+var Order1 = Order{
+	Uid:         "b563feb7b2b84b6test",
+	TrackNumber: "WBILMTESTTRACK",
+	Entry:       "WBIL",
+	Delivery: Delivery{
+		Name:    "Test Testov",
+		Phone:   "+9720000000",
+		Zip:     "2639809",
+		City:    "Kiryat Mozkin",
+		Address: "Ploshad Mira 15",
+		Region:  "Kraiot",
+		Email:   "test@gmail.com",
+	},
+	Payment: Payment{
+		Transaction:  "b563feb7b2b84b6test",
+		RequestId:    "",
+		Currency:     "USD",
+		Provider:     "wbpay",
+		Amount:       1817,
+		PaymentDt:    1637907727,
+		Bank:         "alpha",
+		DeliveryCost: 1500,
+		GoodsTotal:   317,
+		CustomFee:    0,
+	},
+	Items: []Item{
+		{
+			ChrtId:      9934930,
+			TrackNumber: "WBILMTESTTRACK",
+			Price:       453,
+			Rid:         "ab4219087a764ae0btest",
+			Name:        "Mascaras",
+			Sale:        30,
+			Size:        "0",
+			TotalPrice:  317,
+			NmId:        2389212,
+			Brand:       "Vivienne Sabo",
+			Status:      202,
+		},
+	},
+	Locale:            "en",
+	InternalSignature: "",
+	CustomerId:        "test",
+	DeliveryService:   "meest",
+	Shardkey:          "9",
+	SmId:              99,
+	DateCreated:       "2021-11-26T06:22:19Z",
+	OofShard:          "1",
+}
+
+var OrderManyItems = Order{
+	Uid:         "manyItems",
+	TrackNumber: "WBILMTESTTRACK",
+	Entry:       "WBIL",
+	Delivery: Delivery{
+		Name:    "Test Testov",
+		Phone:   "+9720000000",
+		Zip:     "2639809",
+		City:    "Kiryat Mozkin",
+		Address: "Ploshad Mira 15",
+		Region:  "Kraiot",
+		Email:   "test@gmail.com",
+	},
+	Payment: Payment{
+		Transaction:  "manyItems",
+		RequestId:    "",
+		Currency:     "USD",
+		Provider:     "wbpay",
+		Amount:       1817,
+		PaymentDt:    1637907727,
+		Bank:         "alpha",
+		DeliveryCost: 1500,
+		GoodsTotal:   317,
+		CustomFee:    0,
+	},
+	Items: []Item{
+		{
+			ChrtId:      9934930,
+			TrackNumber: "WBILMTESTTRACK",
+			Price:       453,
+			Rid:         "ab4219087a764ae0btest",
+			Name:        "Mascaras",
+			Sale:        30,
+			Size:        "0",
+			TotalPrice:  317,
+			NmId:        2389212,
+			Brand:       "Vivienne Sabo",
+			Status:      202,
+		},
+		{
+			ChrtId:      1234,
+			TrackNumber: "WBILMTESTTRACK",
+			Price:       453,
+			Rid:         "test2",
+			Name:        "item2",
+			Sale:        123,
+			Size:        "0",
+			TotalPrice:  317,
+			NmId:        232452345,
+			Brand:       "lol",
+			Status:      202,
+		},
+	},
+	Locale:            "en",
+	InternalSignature: "",
+	CustomerId:        "test",
+	DeliveryService:   "meest",
+	Shardkey:          "9",
+	SmId:              99,
+	DateCreated:       "2021-11-26T06:22:19Z",
+	OofShard:          "1",
+}
+
+func TestOrderJsonUnMarshal(t *testing.T) {
+	got := Order{}
+	want := Order1
+
+	err := json.Unmarshal(OrderJson1, &got)
+	if err != nil {
+		t.Errorf("Err should be nil, but: %s", err.Error())
+	}
+
+	if diff := deep.Equal(got, want); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func skipDB(t *testing.T) {
+	// if os.Getenv("CI") != "" {
+	// 	t.Skip("Skipping testing in CI environment")
+	// }
+	if false {
+		t.Skip("Skipping tests with db interaction")
+	}
+}
+
+func TestMakeOrderModel(t *testing.T) {
+	skipDB(t)
+
+	c, err := MakeCachedOrderModel()
+	if err != nil {
+		t.Errorf("Err should be nil, but: %s", err.Error())
+	}
+	defer c.Close()
+}
+
+func setRealyUniqueUid(o Order) Order {
+	o.Uid += time.Now().String()
+	return o
+}
+
+func TestInsertGetByUid(t *testing.T) {
+	skipDB(t)
+
+	c, err := MakeCachedOrderModel()
+	if err != nil {
+		t.Fatalf("Err should be nil, but: %s", err.Error())
+	}
+	defer c.Close()
+
+	insertedOrder := setRealyUniqueUid(OrderManyItems)
+	err = c.Insert(insertedOrder)
+	if err != nil {
+		t.Errorf("Err should be nil, but: %s", err.Error())
+	}
+
+	selectedOrder, err := c.GetByUid(insertedOrder.Uid)
+	if err != nil {
+		t.Errorf("Err should be nil, but: %s", err.Error())
+	}
+
+	// изначально у нас не назначен dbid
+	// но GetByUid устанавливает
+	// поэтому их нужно сбить для проверки на равенство
+	selectedOrder.dbId = 0
+	selectedOrder.Payment.dbId = 0
+	selectedOrder.Delivery.dbId = 0
+	for i := 0; i < len(selectedOrder.Items); i++ {
+		selectedOrder.Items[i].dbId = 0
+	}
+
+	if diff := deep.Equal(selectedOrder, &insertedOrder); diff != nil {
+		t.Error(diff)
+	}
+}
