2b63f9b8a7e771a79719fc8bdb4334ca1dd3cc19
Егор Суханов <egor-suhanov2012@yandex.ru>
2023-08-15 23:52:33 +0300

    add task4

--- l2/develop/dev04/task.go
+++ l2/develop/dev04/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +25,47 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func sortString(s string) string {
+	r := []rune(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
+}
 
+func FindAnagramGroups(words []string) map[string][]string {
+	// группируем аннаграммы
+	g := make(map[string][]string)
+	for _, word := range words {
+		word = strings.ToLower(word)
+		anagram := sortString(word)
+		g[anagram] = append(g[anagram], word)
+	}
+
+	// формируем результат
+	res := make(map[string][]string)
+	for _, v := range g {
+		if len(v) < 2 {
+			continue
+		}
+		first := v[0]
+		sort.Strings(v)
+		var vRes []string
+		vRes = append(vRes, v[0])
+		for i := 1; i < len(v); i++ { // добавляем только уникальные слова
+			if v[i-1] != v[i] {
+				vRes = append(vRes, v[i])
+			}
+		}
+		if len(vRes) < 2 { // У нас могло быть много уникальных слов
+			continue
+		}
+		res[first] = vRes
+	}
+	return res
+}
+
+func main() {
+	words := []string{"тяпка", "слиток", "столик", "пятак", "пятка", "листок"}
+	fmt.Println(FindAnagramGroups(words))
 }
