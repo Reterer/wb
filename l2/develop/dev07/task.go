676678875e8358970b5b75ce618dae5a35dde7ef
Егор Суханов <egor-suhanov2012@yandex.ru>
2023-08-17 19:41:10 +0300

    add dev07

--- l2/develop/dev07/task.go
+++ l2/develop/dev07/task.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +40,88 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+// Мультиплексор для done каналов
+// Возвращает канал done, куда сливаются значения из channels
+// Если один из каналов channels закрывается, то канал возвращает nil
+// Если все каналы будут закрыты, то канал done тоже закроется
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	var wg sync.WaitGroup // Если все входящие каналы будут закрыты, то закрываем out
 
+	for _, ch := range channels {
+		wg.Add(1)
+		go func(ch <-chan interface{}) {
+			defer wg.Done()
+			for v := range ch {
+				out <- v // Передаем значение, если пришло какое-то значение
+			}
+			out <- nil // Передаем nil, если канал закрылся
+		}(ch)
+	}
+
+	// Горутина, отвечающая за закрытие канала out
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+// Мультиплексор для done каналов с контекстом ctx
+// Возвращает канал done, куда сливаются значения из channels
+// Если контекст будет отменен, то мультеплексор закроется, включая канал done
+// Если один из каналов channels закрывается, то канал возвращает nil
+// Если все каналы будут закрыты, то канал done тоже закроется
+func ctxor(ctx context.Context, channels ...<-chan interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	var wg sync.WaitGroup // Если все входящие каналы будут закрыты, то закрываем out
+
+	for _, ch := range channels {
+		wg.Add(1)
+		go func(ch <-chan interface{}) {
+			defer wg.Done()
+			for {
+				select {
+				case v, ok := <-ch:
+					out <- v
+					if !ok { // Канал закрылся
+						return
+					}
+				case <-ctx.Done():
+					return
+				}
+			}
+		}(ch)
+	}
+
+	// Горутина, отвечающая за закрытие канала out
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("fone after %v\n", time.Since(start))
 }
--- l2/develop/dev07/task_test.go
+++ l2/develop/dev07/task_test.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func makeChan() chan interface{} {
+	ch := make(chan interface{})
+	return ch
+}
+
+func checkChan(t *testing.T, ch <-chan interface{}, timeout time.Duration, wantOk bool) {
+	wantValue := interface{}(nil)
+	t.Helper()
+	select {
+	case v, ok := <-ch:
+		if v != wantValue {
+			t.Errorf("got: %v want: %v\n", v, wantValue)
+		}
+		if ok != wantOk {
+			t.Errorf("got: %v want: %v\n", ok, wantOk)
+		}
+	case <-time.After(timeout):
+		t.Fatal("timeout")
+	}
+}
+
+func TestOr(t *testing.T) {
+	ch := []chan interface{}{
+		makeChan(),
+		makeChan(),
+		makeChan(),
+	}
+
+	timeout := 10 * time.Millisecond
+
+	done := or(ch[0], ch[1], ch[2])
+	close(ch[0])
+	checkChan(t, done, timeout, true)
+	close(ch[1])
+	checkChan(t, done, timeout, true)
+	close(ch[2])
+	checkChan(t, done, timeout, true)
+
+	checkChan(t, done, timeout, false) // канал done закрыт
+}
+
+func TestCtxor(t *testing.T) {
+	ch := []chan interface{}{
+		makeChan(),
+		makeChan(),
+		makeChan(),
+	}
+
+	timeout := 10 * time.Millisecond
+	ctx, cancel := context.WithCancel(context.TODO())
+
+	done := ctxor(ctx, ch[0], ch[1], ch[2])
+	close(ch[0])
+	checkChan(t, done, timeout, true)
+	close(ch[1])
+	checkChan(t, done, timeout, true)
+
+	cancel()                           // Отменяем контекст
+	checkChan(t, done, timeout, false) // канал done закрыт
+}
