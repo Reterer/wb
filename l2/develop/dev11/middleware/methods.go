4ac1c66e8acdb7096397c98eb24f61e75ded251e
Егор Суханов <egor-suhanov2012@yandex.ru>
2023-08-21 23:27:12 +0300

    add method not allowed

--- l2/develop/dev11/middleware/methods.go
+++ l2/develop/dev11/middleware/methods.go
@@ -9,6 +9,10 @@ func WithMethod(method string, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		if req.Method == method {
 			next.ServeHTTP(w, req)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write([]byte(`{error:"method not allowed"}`))
 		}
 	})
 }
